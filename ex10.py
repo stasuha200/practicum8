d5eba6ec6e9fb76a103b9a7d95be612c0f521e80
Станислав <stasuha200@mail.ru>
2024-04-13 01:32:07 +0700

    ex10

---
+++ ex10.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(2, n+1):
+    s = 1
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            s = s + j + (i / j)
+    if s == i:
+        print(i)
